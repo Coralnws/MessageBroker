2f1327d8b7ed6fe1804eb2b90011a250bbad0ca2
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-04 19:56:33 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/messagebroker/main.go
@@ -0,0 +1,23 @@
+// cmd/messagebroker/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"messagebroker/internal/messagebroker"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := messagebroker.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
